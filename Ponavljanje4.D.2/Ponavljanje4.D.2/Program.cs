007117b9998033397c9ee07d599df9b9f4800fe7
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-09-11 11:46:18 +0200

    Add files via upload

---
+++ Ponavljanje4.D.2/Ponavljanje4.D.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje4.D._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica;
+            Console.WriteLine("Upišite rečenicu sa razmacima");
+            recenica= Console.ReadLine();
+
+            Console.WriteLine(recenica.Replace(" ","_"));
+            Console.ReadKey();
+        }
+    }
+}
